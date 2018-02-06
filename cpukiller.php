c64ab926237c358113988974940f6de3f3228571
Jordi Prats <jordi.prats@gmail.com>
2018-02-06 14:31:22 +0100

    fork cpus

--- cpukiller.php
+++ cpukiller.php
@@ -14,9 +14,36 @@ Author URI: http://systemadmin.es
 
 add_filter('the_content', 'cpukiller_content', 1);
 
-function cpukiller_content($content)
+function get_CPUs()
 {
-  $rand=rand (1,100);
+  $totalCPUs = 1;
+  if (is_file('/proc/cpuinfo'))
+  {
+    $cpuinfo = file_get_contents('/proc/cpuinfo');
+    preg_match_all('/^processor/m', $cpuinfo, $matches);
+    $totalCPUs = count($matches[0]);
+  }
+  else
+  {
+    $process = @popen('sysctl -a', 'rb');
+    if (false !== $process)
+    {
+      $output = stream_get_contents($process);
+      preg_match('/hw.ncpu: (\d+)/', $output, $matches);
+      if ($matches)
+      {
+        $totalCPUs = intval($matches[1][0]);
+      }
+      pclose($process);
+    }
+  }
+
+  return $totalCPUs;
+}
+
+function do_math($content)
+{
+  $rand=rand(1,100);
 
   for($i=0; $i<rand(9999,99999); $i++)
   {
@@ -36,6 +63,27 @@ function cpukiller_content($content)
   }
 
   return $content."<h3><center><strong>".$rand."</strong></center></h3><p><center>CPU killer=true</center></p>";
+}
+
+function cpukiller_content($content)
+{
+  for($i=0; $i<get_CPUs(); $i++)
+  {
+    $pid = pcntl_fork();
+    if ($pid == -1)
+    {
+      die('could not fork');
+    }
+    else if ($pid)
+    {
+      pcntl_wait($status);
+    }
+    else
+    {
+      // child
+      return do_math($content);
+    }
+  }
 }
 
 ?>
