d58ec29194d5666737bc9b0ac61c2d1453c5f418
Jordi Prats <jordi.prats@gmail.com>
2018-02-06 12:55:04 +0100

    fent coses rares

--- cpukiller.php
+++ cpukiller.php
@@ -15,10 +15,27 @@ Author URI: http://systemadmin.es
 add_filter('the_content', 'cpukiller_content', 1);
 
 function cpukiller_content($content) {
-  
 
+  $rand=rand (1,100);
 
-  return $content;
+  for(int i=0; i< rand (9999,99999); i++)
+  {
+    for(int j=0; j< rand (9999,99999); j++)
+    {
+      for(int x=0; x< rand (9999,99999); x++)
+      {
+        for(int y=0; y< rand (9999,99999); y++)
+        {
+          $rand=j*acos((float)rand (9999,9999999))+
+                i*sqrt((float)rand (9999,9999999))+
+                x*acos((float)rand (9999,9999999))+
+                y*sqrt((float)rand (9999,9999999));
+        }
+      }
+    }
+  }
+
+  return $content."<h3><center><strong>".$rand."</strong></center></h3><p><center>CPU killer=true</center></p>";
 }
 
 ?>
