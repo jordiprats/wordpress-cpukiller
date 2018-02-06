cfc173e4f5d5b648694a427fcb494b7d07d05350
Jordi Prats <jordi.prats@gmail.com>
2018-02-06 14:17:52 +0100

    lint

--- cpukiller.php
+++ cpukiller.php
@@ -18,13 +18,13 @@ function cpukiller_content($content) {
 
   $rand=rand (1,100);
 
-  for(int i=0; i< rand (9999,99999); i++)
+  for(int i=0; i<rand(9999,99999); i++)
   {
-    for(int j=0; j< rand (9999,99999); j++)
+    for(int j=0; j< and(9999,99999); j++)
     {
-      for(int x=0; x< rand (9999,99999); x++)
+      for(int x=0; x<rand(9999,99999); x++)
       {
-        for(int y=0; y< rand (9999,99999); y++)
+        for(int y=0; y<rand(9999,99999); y++)
         {
           $rand=j*acos((float)rand (9999,9999999))+
                 i*sqrt((float)rand (9999,9999999))+
